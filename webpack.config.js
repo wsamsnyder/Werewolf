da2fb1f92abdbafd0a67a698fc03ff7348c8ca9e
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-21 00:04:48 -0600

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
   entry: './src/index.js',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  }
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
