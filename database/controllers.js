0fa2cb1af0de3413227c14c287b232c43e312cb3
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-21 23:49:39 -0600

    createRoom- who ever sets it up will be the mod, may need an id just for the mod? createGame - starts the game. takes in a list of names and the roomId. Will need to be refactored to use sockets.

--- database/controllers.js
+++ database/controllers.js
@@ -1,14 +1,53 @@
 const { Room } = require('./schemas');
 
 exports.db = {
-  createRoom: () => {
+  createRoom: (name) => {
+    console.log(name);
     const newRoom = new Room({
-      mod: 'sam',
+      mod: name,
     });
 
-    newRoom.save()
-      .then((val) => {
-        console.log(val);
-      });
+    return newRoom.save();
+  },
+
+  createGame: (namesArr, roomId) => {
+    const wolves = [];
+    let seer = '';
+    let doctor = '';
+    let name = '';
+    const townsPeople = [];
+
+    const names = namesArr.split(',');
+
+    names.forEach((person) => {
+      townsPeople.push({ name: person, alive: true });
+    });
+
+    const numOfWolves = Math.floor(names.length / 5);
+
+    for (let i = 0; i <= numOfWolves + 2; i++) {
+      const randomPlayerIndex = Math.floor(Math.random() * names.length);
+
+      if (wolves.length < numOfWolves) {
+        wolves.push({ name: names.splice(randomPlayerIndex, 1)[0], alive: true });
+      } else if (!seer) {
+        [name] = names.splice(randomPlayerIndex, 1);
+        seer = { name, alive: true };
+      } else {
+        [name] = names.splice(randomPlayerIndex, 1);
+        doctor = { name, alive: true };
+      }
+    }
+
+    // save to db
+    return Room.findByIdAndUpdate(roomId, {
+      wolves,
+      seer,
+      doctor,
+      townsPeople,
+    },
+    {
+      new: true,
+    });
   },
 };
