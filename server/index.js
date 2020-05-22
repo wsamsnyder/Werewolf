fb4773619e37d006e075865c716d149a52a6f918
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-21 23:48:49 -0600

    Two routes set up. Both gets for now but want to move /startGame to a Post. Waiting to get the Sockets set up first.

--- server/index.js
+++ server/index.js
@@ -6,9 +6,31 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/startTime', (req, res) => {
-  //
+app.get('/api/newRoom', (req, res) => {
+  const { name } = req.body;
+
+  db.createRoom(name)
+    .then((val) => {
+      res.status(201).json(val);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+});
+
+app.get('/api/startGame', (req, res) => {
+  const { townsPeople, roomId } = req.body;
+
+  db.createGame(townsPeople, roomId)
+    .then((val) => {
+      res.status(201).json(val);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
