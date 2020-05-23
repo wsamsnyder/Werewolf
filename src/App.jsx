54c74c0681add936a8221aa87e194dc57d67fea3
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-22 22:40:28 -0600

    Actually working now

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import io from 'socket.io-client';
 
 const socket = io.connect();
 
-socket.on('news', (data) => {
-  console.log(data);
-  socket.emit('my other event', { my: 'data' });
-});
+// socket.on('news', (data) => {
+//   console.log(data);
+//   socket.emit('my other event', { my: 'data' });
+// });
 
-const App = ({ socket }) => {
+const App = () => {
+  socket.on('news', (data) => {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+  });
+
   return (
     <div>
       Hello There
