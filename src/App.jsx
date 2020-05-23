3abaa53edb14be6741a63e4b47bf0116e91cd025
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-22 22:39:37 -0600

    Socket.io is up and running.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import io from 'socket.io-client';
 
-const App = () => {
-  console.log('here');
+const socket = io.connect();
+
+socket.on('news', (data) => {
+  console.log(data);
+  socket.emit('my other event', { my: 'data' });
+});
+
+const App = ({ socket }) => {
   return (
     <div>
       Hello There
