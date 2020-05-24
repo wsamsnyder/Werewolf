9bb7c08be4038590120e2aff72d2ce19550de466
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-23 19:21:07 -0600

    set, start are working. Stop isn't properly recieving the interval ID

--- server/Timer.js
+++ server/Timer.js
@@ -1,9 +1,41 @@
-class Timer() {
+class Timer {
   constructor() {
     this.time = 0;
-
+    this.intervalID = null;
   }
 
+  // set
+  set(timeLimit) {
+    // needs to be in seconds
+    this.time = timeLimit;
+  }
+
+  // start -> takes a callback that is the namespace and room to emit on
+  start(callback) {
+    this.intervalID = setInterval(() => {
+      callback(this.time -= 1);
+      if (this.time <= 0) {
+        clearInterval(this.intervalID);
+      }
+    }, 1000);
+  }
+
+  // stop
+  stop() {
+    console.log(this.intervalID);
+    clearInterval(this.intervalID);
+  }
+
+  // add? -> extend current time by x
 }
 
-exports.Timer = Timer;
+let timer = new Timer();
+
+timer.set(10);
+timer.start(console.log);
+
+setTimeout(timer.stop, 3000);
+
+// timer.start(console.log);
+
+// exports.Timer = Timer;
