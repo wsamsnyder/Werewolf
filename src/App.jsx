b7f4dec099c5039776b2f19cdbec30aeaa6daedc
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-25 23:50:15 -0600

    Properly adding the sockets to the state. Need to refactor the chats to be self-contained within each chat module.

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 
 import Socket from './sockets';
 import { api } from './lib';
+import ChatRoom from './chatRoom';
 
 // const socket = io.connect();
 
@@ -12,27 +13,65 @@ import { api } from './lib';
 // });
 
 const App = () => {
-  let namespace;
-  const [namespaceChat, setNamespaceChat] = useState([]);
+  // let namespace;
+  const [townsPeopleChat, setTownsPeopleChat] = useState([]);
+  // these channels should be moved to the messaging module
+  const [wolvesChat, setWolvesChat] = useState([]);
+  const [doctorChat, setDoctorChat] = useState([]);
+  const [seerChat, setSeerChat] = useState([]);
+  const [sockets, setSockets] = useState({});
+  const [room, setRoom] = useState('');
   // let [townsPeopleChat, setTownsPeopleChat] = useState([]);
 
   // make a new room with the namespace of the id returned
   const createNamespace = () => {
     api.createNamespace('sam')
-      .then((roomID) => {
-        namespace = new Socket(roomID, 'sam');
-        namespace.joinNamespace((message) => (
-          // may make this perm if it's something that will be done often
-          // eslint-disable-next-line no-shadow
-          setNamespaceChat((namespaceChat) => [...namespaceChat, message])
-        ));
+      .then((namespaces) => {
+        setRoom(namespaces[0].id);
+        namespaces.forEach(({ chatName, id }) => {
+          if (chatName === 'townsPeopleChat') {
+            const chat = new Socket(id, 'sam');
+            setSockets((allSockets) => ({ ...allSockets, townsPeople: chat }));
+            chat.joinNamespace((message) => (
+              setTownsPeopleChat((previousMessages) => [...previousMessages, message])
+            ));
+          }
+          if (chatName === 'wolvesChat') {
+            const chat = new Socket(id, 'sam');
+            setSockets((allSockets) => ({ ...allSockets, wolves: chat }));
+            chat.joinNamespace((message) => (
+              setWolvesChat((previousMessages) => [...previousMessages, message])
+            ));
+          }
+          if (chatName === 'doctorChat') {
+            const chat = new Socket(id, 'sam');
+            setSockets((allSockets) => ({ ...allSockets, doctor: chat }));
+            chat.joinNamespace((message) => (
+              setDoctorChat((previousMessages) => [...previousMessages, message])
+            ));
+          }
+          if (chatName === 'seerChat') {
+            const chat = new Socket(id, 'sam');
+            setSockets((allSockets) => ({ ...allSockets, seer: chat }));
+            chat.joinNamespace((message) => (
+              setSeerChat((previousMessages) => [...previousMessages, message])
+            ));
+          }
+        });
       });
+  };
+
+  const sendMessage = () => {
+    sockets.townsPeople.sendMessage('hello There');
   };
 
   return (
     <div>
       <button type="button" onClick={createNamespace}>Create Namespace</button>
-      <button type="button">{namespaceChat}</button>
+      <button type="button">{townsPeopleChat}</button>
+      <div>{room}</div>
+      <ChatRoom roomName={sockets} />
+      <button type="button" onClick={sendMessage}>Send Message</button>
     </div>
   );
 };
