5ecd247505bbc72a9137ae64a19767b866f55615
diversifiedmechanic <diversifiedmechanic01@gmail.com>
2020-05-26 23:42:00 -0600

    Pulled out socket connections. Storing in the state as an array. Needs to be in state for when regular players are assigned a role.

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
 import React, { useState } from 'react';
-// import io from 'socket.io-client';
 
 import Socket from './sockets';
 import { api } from './lib';
 import ChatRoom from './chatRoom';
 
-// const socket = io.connect();
-
-// socket.on('news', (data) => {
-//   console.log(data);
-//   socket.emit('my other event', { my: 'data' });
-// });
-
 const App = () => {
   // these channels should be moved to the messaging module
-  const [townsPeopleChat, setTownsPeopleChat] = useState([]);
-  const [wolves, setWolvesChat] = useState([]);
-  const [doctor, setDoctorChat] = useState([]);
-  const [seer, setSeerChat] = useState([]);
-  const [sockets, setSockets] = useState({});
+  // const [townsPeopleChat, setTownsPeopleChat] = useState([]);
+  // const [wolves, setWolvesChat] = useState([]);
+  // const [doctor, setDoctorChat] = useState([]);
+  // const [seer, setSeerChat] = useState([]);
+  const [sockets, setSockets] = useState([]);
   const [room, setRoom] = useState('');
   // let [townsPeopleChat, setTownsPeopleChat] = useState([]);
 
@@ -27,50 +19,60 @@ const App = () => {
     api.createGameRoom('sam')
       .then(({ gameId, chatRooms }) => {
         setRoom(gameId);
-        console.log(chatRooms);
+        const rooms = [];
         chatRooms.forEach(({ roomName, roomId }) => {
-          if (roomName === 'townsPeople') {
-            const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
-            setSockets((allSockets) => ({ ...allSockets, townsPeople: chat }));
-            chat.joinNamespace((message) => (
-              setTownsPeopleChat((previousMessages) => [...previousMessages, message])
-            ));
-          }
-          if (roomName === 'wolves') {
-            const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
-            setSockets((allSockets) => ({ ...allSockets, wolves: chat }));
-            chat.joinNamespace((message) => (
-              setWolvesChat((previousMessages) => [...previousMessages, message])
-            ));
-          }
-          if (roomName === 'doctor') {
-            const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
-            setSockets((allSockets) => ({ ...allSockets, doctor: chat }));
-            chat.joinNamespace((message) => (
-              setDoctorChat((previousMessages) => [...previousMessages, message])
-            ));
-          }
-          if (roomName === 'seer') {
-            const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
-            setSockets((allSockets) => ({ ...allSockets, seer: chat }));
-            chat.joinNamespace((message) => (
-              setSeerChat((previousMessages) => [...previousMessages, message])
-            ));
-          }
+          rooms.push({
+            roomId,
+            username: 'username',
+            userId: roomId,
+            gameId,
+            roomName,
+          });
+
+          // if (roomName === 'townsPeople') {
+            // const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
+          //   setSockets((allSockets) => ([ ...allSockets, townsPeople: chat ]));
+          //   chat.joinNamespace((message) => (
+          //     setTownsPeopleChat((previousMessages) => [...previousMessages, message])
+          //   ));
+          // }
+          // if (roomName === 'wolves') {
+          //   const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
+          //   setSockets((allSockets) => ([ ...allSockets, wolves: chat ]));
+          //   chat.joinNamespace((message) => (
+          //     setWolvesChat((previousMessages) => [...previousMessages, message])
+          //   ));
+          // }
+          // if (roomName === 'doctor') {
+          //   const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
+          //   setSockets((allSockets) => ([ ...allSockets, doctor: chat ]));
+          //   chat.joinNamespace((message) => (
+          //     setDoctorChat((previousMessages) => [...previousMessages, message])
+          //   ));
+          // }
+          // if (roomName === 'seer') {
+          //   const chat = new Socket(roomId, 'sam', roomId, gameId, roomName);
+          //   setSockets((allSockets) => ([ ...allSockets, seer: chat ]));
+          //   chat.joinNamespace((message) => (
+          //     setSeerChat((previousMessages) => [...previousMessages, message])
+          //   ));
+          // }
         });
       });
   };
 
-  const sendMessage = () => {
-    sockets.townsPeople.sendMessage('hello There');
-  };
+
 
   return (
     <div>
       <button type="button" onClick={createGameRoom}>Create Namespace</button>
       <div>{townsPeopleChat}</div>
       <div>{room}</div>
-      <ChatRoom roomName={sockets} />
+      {
+        sockets.map((socket) => (
+          <ChatRoom  />
+        ))
+      }
       <button type="button" onClick={sendMessage}>Send Message</button>
     </div>
   );
